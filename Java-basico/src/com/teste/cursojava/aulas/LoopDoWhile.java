a329a81f6bfdc9248c3272487cee3721c734c8da
mailsondias7 <mailsondias7@gmail.com>
2021-02-13 15:32:45 -0300

    Aula LoopWhile e DoWhile

---
+++ Java-basico/src/com/teste/cursojava/aulas/LoopDoWhile.java
@@ -0,0 +1,20 @@
+package com.teste.cursojava.aulas;
+
+public class LoopDoWhile {
+
+	public static void main(String[] args) {
+		
+		int i = 10;
+		int max = 21;
+		
+		do {
+			i++;
+			System.out.println("Valor de i: " + i);
+		}while(i<max); 
+		
+			System.out.println(i);
+			
+		}
+
+	}
+
