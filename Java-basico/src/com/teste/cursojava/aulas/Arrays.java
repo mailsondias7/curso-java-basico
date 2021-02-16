8139895595535a8027a9382b8d4b2f728b73c6dd
mailsondias7 <mailsondias7@gmail.com>
2021-02-16 12:39:50 -0300

    Aula sobre Arrays

---
+++ Java-basico/src/com/teste/cursojava/aulas/Arrays.java
@@ -0,0 +1,20 @@
+package com.teste.cursojava.aulas;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+
+		double[] temperaturas = new double[365];
+		temperaturas[0] = 31.3;
+		temperaturas[1] = 30.2;
+
+		System.out.println("Valor da temperatura do dia 1: " + temperaturas[0]);
+
+		System.out.println("Tamanho do Array: " + temperaturas.length);
+
+		for (int i = 0; i < temperaturas.length; i++) {
+			System.out.println("O valor da temperatura no dia " + (i+1) + " é: " + temperaturas[i]);
+		}
+	}
+
+}
